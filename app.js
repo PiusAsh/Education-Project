bbf47dad5a9b3eabe337563f17daf9bd367779c1
PiusAsh <Pius1ash@gmail.com>
2022-04-15 08:16:46 +0100

    Adjustd the login page

--- app.js
+++
@@ -1,9 +0,0 @@
-function showDiv() {
-    document.getElementById('input3').style.display = "none";
-    document.getElementById('loadingGif').style.display = "block";
-    setTimeout(function () {
-        document.getElementById('loadingGif').style.display = "none";
-        document.getElementById('showme').style.display = "block";
-    }, 2000);
-
-}
